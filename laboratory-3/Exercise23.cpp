da494e7c985df55a5b90261c994b7d09419f844c
Adrian Ściepura <adriansciepura@outlook.com>
2022-10-16 21:40:28 +0200

    Update Exercise23.cpp
    working version

--- laboratory-3/Exercise23.cpp
+++ laboratory-3/Exercise23.cpp
@@ -1,8 +1,8 @@
-﻿#include <iostream>
+#include <iostream>
 
 using namespace std;
 
-void printBinary(unsigned char x)
+void print_binary(unsigned char x)
 {
     int b;
     for (b = 2 * sizeof(int) - 1; b >= 0; b--)
@@ -11,12 +11,12 @@ void printBinary(unsigned char x)
     }
 }
 
-int getSpecificBinary(unsigned char x, int pos)
+int get_specific_position(unsigned char x, int pos)
 {
     int b;
     for (b = 2 * sizeof(int) - 1; b >= 0; b--)
     {
-        if (b != 8-pos)
+        if (b != pos-1)
         {
             continue;
         }
@@ -24,14 +24,12 @@ int getSpecificBinary(unsigned char x, int pos)
     }
 }
 
-
 int main()
 {
     unsigned char z = 0x00;
     const int num_of_players = 4;
     int score[num_of_players];
     int previous_throw[num_of_players];
-    bool is_ready[num_of_players];
     bool end_of_game = false;
     int board_size;
     cin >> board_size;
@@ -44,7 +42,6 @@ int main()
     {
         score[j] = 0;
         previous_throw[j] = 0;
-        is_ready[j] = false;
     }
 
     while (!end_of_game)
@@ -58,41 +55,52 @@ int main()
                 i = num_of_players-1;
             }
 
-        case 'M':
-        {
-            int number;
-            cin >> number;
-            if (getSpecificBinary(z, 2*i+2) == 1)
+            case 'M':
             {
-                score[i] += number;
-                if (score[i] >= board_size)
+                int number;
+                cin >> number;
+                if (get_specific_position(z, 2*i+2) == 1)
                 {
-                    cout << "END OF GAME";
-                    end_of_game = true;
+                    score[i] += number;
+                    if (score[i] % 2 != 0)
+                    {
+                        int temp = pow(2, i * 2);
+                        z = z | temp;
+                    }
+                    else
+                    {
+                        int temp = 255 - pow(2, i * 2);
+                        z = z & temp;
+                    }
+
+                    if (score[i] >= board_size)
+                    {
+                        cout << "END OF GAME";
+                        end_of_game = true;
+                    }
+                }
+                else if (number == 6 && previous_throw[i] == 1)
+                {
+                    int temp = pow(2, (i + 1) * 2 - 1);
+                    z = z | temp;
+                }
+                previous_throw[i] = number;
+                i--;
+                break;
+            }
+            case 'P':
+            {
+                i = num_of_players-1;
+
+                for (int j = num_of_players - 1; j >= 0; j--)
+                {
+                    cout << score[j] << ' ';
                 }
 
+                print_binary(z);
+                cout << '\n';
+                break;
             }
-            else if (number == 6 && previous_throw[i] == 1)
-            {
-                //is_ready[i] = true;
-                int temp = pow(2, (i + 1) * 2 - 1);
-                z = z | temp;
-            }
-            previous_throw[i] = number;
-            i--;
-            break;
-        }
-        case 'P':
-        {
-            i = num_of_players-1;
-            for (auto sc : score)
-            {
-                cout << sc << " ";
-            }
-            printBinary(z);
-            cout << '\n';
-            break;
-        }
         }
     }
 }
